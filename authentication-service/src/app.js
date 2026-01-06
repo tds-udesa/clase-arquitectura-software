8b1a01686aab30ecc9ce11a5d92c05b0549ced48
avillores-qontigo <Alejo.Villores@simcorp.com>
2026-01-06 17:39:52 -0300

    Add Swagger page to Auth Service

--- authentication-service/src/app.js
+++ authentication-service/src/app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = require('./routes/auth')
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('./swagger-output.json');
 
 const app = express()
 app.use(express.json())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router)
 
--- authentication-service/src/handlers/auth.js
+++ authentication-service/src/handlers/auth.js
@@ -2,6 +2,19 @@ const { authenticateUser } = require('../services/auth')
 const { validationResult } = require('express-validator');
 
 async function loginHandler(req, res) {
+    /*
+        #swagger.tags = ['Authentication']
+        #swagger.description = 'Endpoint to login a user and receive an access token.'
+        #swagger.parameters['body'] = {
+            in: 'body',
+            description: 'User credentials.',
+            required: true,
+            schema: {
+                username: 'user1',
+                password: 'password123'
+            }
+        }
+    */
     const result = validationResult(req);
 
     if (!result.isEmpty()) {
